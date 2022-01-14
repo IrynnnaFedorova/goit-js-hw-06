397f7251b25bd9306f52c743d482fbf0dcd3ce89
IrynnnaFedorova <bulhakovairynnna@gmail.com>
2022-01-14 21:14:41 +0200

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsEl = document.querySelector('#ingredients')
+const ingredientsArray = ingredients.map(element => {
+  const ingredientList = document.createElement('li');
+  ingredientList.classList.add('item');
+  ingredientList.textContent = element;
+
+  return ingredientList;
+});
+
+ingredientsEl.append(...ingredientsArray);
