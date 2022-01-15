24740e6d7e3ff05279f40e35c10572e7cb6ea72a
IrynnnaFedorova <bulhakovairynnna@gmail.com>
2022-01-15 20:13:12 +0200

    task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
-
+const loginEl = document.querySelector('form.login-form');
+console.log(loginEl);
+loginEl.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements; 
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    if (email == '' || password == '') {
+        alert("Enter both input parameters!");
+        loginEl.reset();
+        return false;
+    }
+    const formData = {
+        email,
+        password,
+    }
+    console.log(formData);
+    
+    loginEl.reset();
+} 
